d8170e2b25bb84779c677cb2fc285abf97833c5d
Alim674-a <zhetteev67@mail.ru>
2026-04-24 16:12:25 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-# Railway provides PORT. Serve static files from current directory.
+# Railway provides PORT. `serve` v14 expects endpoint format.
 PORT="${PORT:-3000}"
+LISTEN_ENDPOINT="tcp://0.0.0.0:${PORT}"
 
-exec npx --yes serve@14.2.4 -s . -l "$PORT"
+exec npx --yes serve@14.2.4 -s . -l "$LISTEN_ENDPOINT"
 
